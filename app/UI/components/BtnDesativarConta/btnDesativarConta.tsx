f1422ace5fc0fd4a21378383c4f1f4145f0b66f7
Enzo-1022 <enzofernandes560@gmail.com>
2026-03-11 17:29:26 -0300

    Finalização dos testes do endpoint de desativar usuarios
    Fiz o fetch que realiza a requisição ao endpoint da api que desativa o perfil do usuário.

--- app/UI/components/BtnDesativarConta/btnDesativarConta.tsx
+++ app/UI/components/BtnDesativarConta/btnDesativarConta.tsx
@@ -1,10 +1,45 @@
-import desativaUSuario from "./tt"
+'use client'
+import { useRouter } from "next/navigation";
 
-export default async function BtnDesativarConta() {
-    return (
+export default function BtnDesativarConta() {
     
-    <form action={desativaUSuario}>
-        <button type="submit">Desativar Conta?</button>
-    </form>
- )
+    async function teste() {
+        try {
+            // const router = useRouter();
+            var cookies = document.cookie;
+            var token = cookies.split('sessionToken=')[1]
+            // var token = cookies
+
+            const Response = await fetch(
+                'http://localhost:3001/Usuarios/DesativarPerfil',
+                {
+                    method : 'PATCH',
+                    mode : "cors",
+                    credentials : 'include',
+                    headers : {
+                        'Content-Type' : 'application/json', 
+                        'authorization' : `Bearer ${token}`,
+                        'accept' : 'application/json'
+                    }
+                }
+            );
+
+            // switch (Response.status) {
+            //     case 204:
+            //         router.push('/');
+            //         break;
+            
+            //     default:
+            //         console.log(Response.status, Response.statusText);
+            //         break;
+            // }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+        
+
+    return (
+        <button onClick={() => { teste(), console.log('foi') }} type="submit">Desativar conta?</button>
+    )
 }
